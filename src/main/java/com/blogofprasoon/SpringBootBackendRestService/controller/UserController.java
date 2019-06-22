577f1ab70826fb2b0a77d8b7179bdbc5feaba0cc
Prasoon <prasoonparashar49@gmail.com>
2019-06-22 12:10:55 +0530

    added plain Java class for the controller

---
+++ src/main/java/com/blogofprasoon/SpringBootBackendRestService/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.blogofprasoon.SpringBootBackendRestService.controller;
+
+import com.blogofprasoon.SpringBootBackendRestService.model.User;
+import com.blogofprasoon.SpringBootBackendRestService.service.UserService;
+
+import java.util.List;
+
+public class UserController {
+
+    UserService userService;
+
+    public List<User>  getAllUsers() {
+        return null;
+    }
+
+    public User getUserById(Long id) {
+        return null;
+    }
+    public User createUser(User user) {
+        return null;
+    }
+
+    public User updateUser(User user) {
+        return null;
+    }
+
+    public User deleteUser(User user) {
+        return null;
+    }
+}
