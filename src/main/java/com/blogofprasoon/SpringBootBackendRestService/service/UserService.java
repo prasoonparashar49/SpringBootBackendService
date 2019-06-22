22eb5c0e061d4ce71d94a4fc3c7cc059f59c6276
Prasoon <prasoonparashar49@gmail.com>
2019-06-22 11:23:29 +0530

    added anotation marking UserService as a bean, so that it becomes visible to controller

--- src/main/java/com/blogofprasoon/SpringBootBackendRestService/service/UserService.java
+++ src/main/java/com/blogofprasoon/SpringBootBackendRestService/service/UserService.java
@@ -2,9 +2,11 @@ package com.blogofprasoon.SpringBootBackendRestService.service;
 
 import com.blogofprasoon.SpringBootBackendRestService.model.User;
 import com.blogofprasoon.SpringBootBackendRestService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserService {
 
     UserRepository userRepository;
