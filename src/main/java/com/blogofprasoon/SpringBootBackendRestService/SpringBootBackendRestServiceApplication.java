da8179fa1fadbaae4234f081680b38a871f685a5
Prasoon <prasoonparashar49@gmail.com>
2019-06-22 10:26:11 +0530

    adding initial codebase

---
+++ src/main/java/com/blogofprasoon/SpringBootBackendRestService/SpringBootBackendRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogofprasoon.SpringBootBackendRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogofprasoon/SpringBootBackendRestService/SpringBootBackendRestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogofprasoon.SpringBootBackendRestService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBackendRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
